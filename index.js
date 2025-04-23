b042d982059b8415a0fa1a2476e5fbaeb55a3144
ranandasatria <rananda24@gmail.com>
2025-04-23 14:02:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-import moment from "moment"
-import readline from "readline"
-
-// minitask
-// - Membuat program interaktif bernama "convert tanggal" untuk mengubah input tanggal 24-04-2015
-// menjadi 24/04/2015 menggunakan package moment
-// - Jika input user tidak sesuai format. maka outputkan pesan "Format tanggal salah"
-// - Membuat padanan program tanpa external package, tapi dengan menggunakan alur sendiri
+import moment from "moment";
+import readline from "readline";
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout
+});
 
 rl.question(`Silakan masukkan tanggal [DD-MM-YYYY]: `, (input)=>{
-    const inputFormat = moment(input, "DD-MM-YYYY", true)
-    if(inputFormat.isValid()){
-        const changeFormat = inputFormat.format("DD/MM/YYYY")
-        console.log(changeFormat)
-    }else{
-        console.log("Format tanggal salah.")
-    }
-    
-rl.close()
-})
-
-
+  const inputFormat = moment(input, "DD-MM-YYYY", true);
+  if(inputFormat.isValid()){
+    const changeFormat = inputFormat.format("DD/MM/YYYY");
+    console.log(changeFormat);
+  }else{
+    console.log("Format tanggal salah.");
+  } 
+  rl.close();
+});
