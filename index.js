692801eda890afe55e51f60c4b037e97aa035a65
ranandasatria <rananda24@gmail.com>
2025-04-23 22:33:10 +0700

    update manual tanpa package

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// Menggunakan package
+
 function convertDate(){
   rl.question(`Silakan masukkan tanggal [DD-MM-YYYY]: `, (input)=>{
     try{
@@ -14,12 +16,35 @@ function convertDate(){
         const changeFormat = inputFormat.format("DD/MM/YYYY");
         console.log(changeFormat);
       }else {
-        throw new Error("Format tanggal salah.");
+        throw new Error("format_salah");
       } 
-    } catch (err) {
+    } catch (Err) {
       console.log("Masukkan tanggal sesuai format.");
     }
     rl.close();
   });
 }
-convertDate();
+
+// Manual tanpa package
+// function convertDate(){
+//   rl.question(`Silakan masukkan tanggal [DD-MM-YYYY]: `, (input) => {
+//     try{
+//     const wadah = input.split("-")
+
+//     if(wadah.length !== 3 || wadah[0] < "01" || wadah[0] > "31" || 
+//       wadah[1] < "01" || wadah[1] > "12"){
+//       const err = new Error("format_salah")
+//       throw err
+//     }
+//     const newFormat = wadah.join("/");
+//     console.log(newFormat);
+//     }catch(err) {
+//       if(err.message === "format_salah"){
+//         console.log("Masukkan tanggal sesuai format.")
+//       }
+//     }
+//     rl.close()
+//   })
+// }
+
+convertDate()
